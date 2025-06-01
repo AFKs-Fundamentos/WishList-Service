234bafa00cc52ecaf00d83706a62401888c4056b
diego5m <diegosoriano137@gmail.com>
2025-05-31 21:46:15 -0500

    feat: configuration

---
+++ src/test/java/com/pcmaster/afk/wishlist/WishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcmaster.afk.wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
